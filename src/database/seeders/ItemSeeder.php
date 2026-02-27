9cad78c30e3a49392b2b39ad088c46f09be1d39a
kay4300 <alltoowell22red@gmail.com>
2026-02-27 22:30:31 +0900

    2026.2.27更新再

--- src/database/seeders/ItemSeeder.php
+++ src/database/seeders/ItemSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use App\Models\Item;
 use App\Models\User;
+use App\Models\Category;
 
 class ItemSeeder extends Seeder
 {
@@ -13,6 +14,7 @@ class ItemSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
         $user = User::first();
@@ -20,6 +22,30 @@ class ItemSeeder extends Seeder
             'user_id' => User::first()->id
 
         ]);
+
+        $items = [
+            'タンブラー' => ['キッチン'],
+            'グラス' => ['キッチン'],
+            'ペン' => ['アクセサリー'],
+            'ヘッドフォン' => ['家電', 'アクセサリー'],
+            '一人用ソファ' => ['インテリア'],
+            'カトラリーセット' => ['キッチン'],
+            '食器' => ['キッチン'],
+            'ショルダーバッグ' => ['ファッション', 'アクセサリー', 'レディース'],
+            'ヘアドライヤー' => ['家電'],
+            'ノートパソコン' => ['家電'],
+
+        ];
+
+        foreach ($items as $itemName => $categoryNames) {
+
+            $item = Item::where('name', $itemName)->first();
+
+            $categoryIds = Category::whereIn('name', $categoryNames)
+                ->pluck('id');
+
+            $item->categories()->sync($categoryIds);
         //
+        }
     }    
 }
