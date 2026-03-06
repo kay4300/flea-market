466bfe5a8d3b1130c1443a18c82a7cb3562a9cf2
kay4300 <alltoowell22red@gmail.com>
2026-03-06 23:22:27 +0900

    2026.3.6更新

--- src/app/Http/Controllers/PurchaseController.php
+++ src/app/Http/Controllers/PurchaseController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Models\Purchase;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Profile;
+use App\Models\Item;
 
 class PurchaseController extends Controller
 {
@@ -41,7 +43,31 @@ class PurchaseController extends Controller
             'item_id' => $itemId,
         ]);
 
-        return redirect()->route('mypage'); //
+        return redirect()->route('mypage'); 
+    }
+
+    public function editProfile()
+    {
+        $profile = Profile::where('user_id', Auth::id())->firstOrFail();
+        return view('profile', compact('profile'));
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $userId = Auth::id();
+
+        $profile = Profile::where('user_id', $userId)->firstOrFail();
+
+        // バリデーション
+        $validated = $request->validate([
+            'postcode' => 'required|string|max:10',
+            'address' => 'required|string|max:255',
+        ]);
+
+        $profile->update($validated);
+
+        // 更新後、購入ページに戻す
+        return redirect()->route('purchase.show', session('purchase_item_id'));
     }
 
     /**
@@ -50,10 +76,33 @@ class PurchaseController extends Controller
      * @param  \App\Models\Purchase  $purchase
      * @return \Illuminate\Http\Response
      */
-    public function show(Purchase $purchase)
+    public function show($itemId)
     {
+        $item = Item::findOrFail($itemId);
+        $profile = Profile::where('user_id', Auth::id())->firstOrFail();
+
+        // ← ここで session に保存
+        session(['purchase_item_id' => $itemId]);
+
+        return view('purchase', compact('item', 'profile'));
         //
     }
+
+    public function purchase($id)
+    {
+        $item = Item::findOrFail($id);
+
+        if ($item->is_sold) {
+            return back();
+        }
+
+        $item->is_sold = true;
+        $item->buyer_id = auth()->id();
+        $item->save();
+
+        return redirect()->route('purchase.show', $item->id);
+    }
+
 
     /**
      * Show the form for editing the specified resource.
